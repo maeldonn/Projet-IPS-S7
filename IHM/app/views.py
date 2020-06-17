4b21fc5a3ea4a230e371e072a556d5304ff7152b
Maël Donnart <57325246+maeldonn@users.noreply.github.com>
2020-06-17 15:19:05 +0200

    changes

--- IHM/app/views.py
+++ IHM/app/views.py
@@ -16,22 +16,16 @@ def get_temp(sauvegardeStatus):
 
 def enregistrement(temperature):
     d = datetime.now()
-    # On recupere de datetime.now l'annee & le jour et on le stocke
     annee = str(d)[:4]
     jour = str(d)[:10]
     repertoire = './temperatures/' + annee + '/'
-    # On verifie que le repertoire existe. S'il n'existe pas : on le cree
     if not os.path.isdir(repertoire):
         os.makedirs(repertoire)
     fich_Temp = repertoire + jour + '.txt'
-    # On ouvre le fichier ayant pour nom la date_du_jour.txt en fonction de s'il existe ou pas : en add ou en write
     if os.path.exists(fich_Temp):
-        # Le fichier existe, on l'ouvre en mode ajout
         f = open(fich_Temp, "a")
     else:
-        # Le fichier n'existe pas, on le cree et on l'ouvre
         f = open(fich_Temp, "w")
-    # Enfin, on ajoute une ligne dans le fichier au format : HH:MM Temp _ Exemple : 10:15 27.02
     time = d.strftime("%H:%M")
     print(str(time) + " " + str(temperature), file=f)
     f.close()
@@ -63,13 +57,10 @@ def index():
 @app.route("/<date>")
 def graphTemp(date):
     if date is None:
-        # On a entre 192.xx.xx.xx/graphTemp sans indiquer de date, on affiche donc le graph du jour
         d = datetime.now()
-        titre = "Temperature actuelle : " + str(get_temp(False))
+        titre = "Température actuelle : " + str(get_temp(False)) + " °C"
     else:
-        # Sinon, on affiche le graph de la date donnee | Ex : 192.xx.xx.xx/graphTemp/20180618
         d = datetime.strptime(date, '%Y%m%d')
-        # Les 6 prochaines lignes permettent d'avoir un titre type : 08/09/2018 au lieu de 8/9/2018
         strMonth = str(d.month)
         strDay = str(d.day)
         if len(strMonth) == 1:
@@ -78,16 +69,11 @@ def graphTemp(date):
             strDay = "0" + strDay
         dateTitle = strDay + "/" + strMonth + "/" + str(d.year)
         titre = "Statistiques du " + dateTitle
-    # Liste contenant deux liste :
-    # pointsAndTemp[0] : coordonnees des points du graph;
-    # pointsAndTemp[1] : temperatures, permet de faire des stats : max,min,moy
     pointsAndTemp = fichierVersListe(
         "./temperatures/" + str(d)[:4] + "/" + str(d)[:10] + ".txt")
     temperatures = pointsAndTemp[1]
-    # On calcule des dates precedantes et suivantes pour la navigation entre les jours
     nextDate = d + timedelta(days=1)
     prevDate = d - timedelta(days=1)
-    # On retourne tout a la page graphTemp.html
     return render_template(
         'index.html',
         d=d.strftime("%Y-%m-%d"),
